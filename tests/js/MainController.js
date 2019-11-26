a5685a532be694f9179809ad8c3a36de557b9310
sezzerouali <sezzerouali@ncelrnd0886.nce.amadeus.net>
2019-11-26 14:57:42 +0100

    https://www.youtube.com/watch?v=l23KYiM5Se0

--- tests/js/MainController.js
+++ tests/js/MainController.js
@@ -1,3 +1,6 @@
 app.controller('MainController',['$scope',function($scope){
     $scope.list = ["call free","mail dufeutrell","go joney go"];
+    $scope.addItem = function(){
+        $scope.list.push($scope.addToDo);
+    }
 }])
